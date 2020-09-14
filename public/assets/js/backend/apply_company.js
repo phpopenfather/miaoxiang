d07c0eee3b1765fb54a36e69f846b3faae741bc5
7TB3TIPOMRDKFDE\Administrator <phpopenfather@163.com>
2020-09-14 18:51:52 +0800

    企业申请后台

---
+++ public/assets/js/backend/apply_company.js
@@ -0,0 +1,55 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'apply_company/index' + location.search,
+                    add_url: 'apply_company/add',
+                    edit_url: 'apply_company/edit',
+                    del_url: 'apply_company/del',
+                    multi_url: 'apply_company/multi',
+                    import_url: 'apply_company/import',
+                    table: 'apply_company',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'co_name', title: __('Co_name')},
+                        {field: 'name', title: __('Name')},
+                        {field: 'phone', title: __('Phone')},
+                        {field: 'mail', title: __('Mail')},
+                        {field: 'apply_time', title: __('Apply_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
